935f75dbbd269c6334ecd27f8ebb66931b29331c
Mirosław Mroczkowski <mroczkowski.miroslaw@gmail.com>
2021-04-19 19:30:28 +0200

    responsivness, hamburger etc

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
-const name = "Mirek";
-const age = 37;
+// const myName = "Mirek";
+// const age = 37;
 
-function greet(age, name) {
-  alert(`Cześć, jestem ${name}, mam ${age} lat. Zapraszam Cię na moją stronę`);
-}
+// function greet(age, myName) {
+//   alert(`Cześć, jestem ${myName}, mam ${age} lat. Zapraszam Cię na moją stronę`);
+// }
 
-greet(age, name);
+// greet(age, myName);
+
+const burger = document.querySelector('.burger');
+
+const burgerIcon = document.querySelector('.fa-bars');
+const timesIcon = document.querySelector('.fa-times');
+const navigation = document.querySelector('.navigation');
+const headerBurger = document.querySelector('.header__burger');
+
+
+burger.addEventListener('click', () => {
+ burgerIcon.classList.toggle('active');
+ timesIcon.classList.toggle('active');
+ navigation.classList.toggle('active');
+ headerBurger.classList.toggle('active');
+});
